fbad705bd40feb86f8393f686bf77909098a5bc3
yakirk <kadkoda@gmail.com>
2016-11-19 00:49:28 +0200

    fix (eBay Code): rename stuff and shit

--- FinalProject/Controllers/BuyController.cs
+++ FinalProject/Controllers/BuyController.cs
@@ -3,11 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
-using System.Linq;
 using System.Net;
+using FinalProject.Models;
 using System.IO;
 using System.Text;
 using Newtonsoft.Json.Linq;
@@ -17,11 +14,29 @@ namespace FinalProject.Controllers
 {
     public class BuyController : Controller
     {
-        public class EbayItem
+        public ActionResult Index(int? id)
         {
-            public string name { get; set; }
-            public string url { get; set; }
-            public string price { get; set; }
+            string EbayRequest = "http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=YakirKad-Kadkoda-PRD-645e91339-73768646&GLOBAL-ID=EBAY-US&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords=" +
+                                 "Computer Programming Books" + "&paginationInput.entriesPerPage=20";
+
+            var result = JObject.Parse(GET(EbayRequest));
+            int numberOfResults = Convert.ToInt32(result["findItemsByKeywordsResponse"][0]["searchResult"][0]["@count"].ToString());
+            var EbayListItem = new List<Buy.EbayItem>();
+
+            for (int index = 0; index < numberOfResults; index++)
+            {
+                Buy.EbayItem item = new Buy.EbayItem();
+    
+                item.ProductName = result["findItemsByKeywordsResponse"][0]["searchResult"][0]["item"][index]["title"][0].ToString();
+                item.URL         = result["findItemsByKeywordsResponse"][0]["searchResult"][0]["item"][index]["viewItemURL"][0].ToString();
+                item.Price       = result["findItemsByKeywordsResponse"][0]["searchResult"][0]["item"][index]["sellingStatus"][0]["currentPrice"][0]["__value__"].ToString();
+
+                EbayListItem.Add(item);
+            }
+
+            ViewBag.ebayResuls = EbayListItem;
+
+            return View();
         }
 
         // Makes an http get request and returns the result.
@@ -44,41 +59,9 @@ namespace FinalProject.Controllers
                 {
                     StreamReader reader = new StreamReader(responseStream, Encoding.GetEncoding("utf-8"));
                     String errorText = reader.ReadToEnd();
-                    // log errorText
                 }
                 throw;
             }
-        }
-
-        public ActionResult Index(int? id)
-        {
-
-
-
-            string EbayRequest = "http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=YakirKad-Kadkoda-PRD-645e91339-73768646&GLOBAL-ID=EBAY-US&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords=" +
-               "Computer Programming Books" + "&paginationInput.entriesPerPage=20";
-
-            // parse the string from the request into a json object
-            var result = JObject.Parse(GET(EbayRequest));
-
-            int numberOfResults = Convert.ToInt32(result["findItemsByKeywordsResponse"][0]["searchResult"][0]["@count"].ToString());
-            List<EbayItem> items = new List<EbayItem>();
-
-            // Create from a json a list of ebay recomandation
-            for (int i = 0; i < numberOfResults; i++)
-            {
-                EbayItem item = new EbayItem();
-    
-                item.name = result["findItemsByKeywordsResponse"][0]["searchResult"][0]["item"][i]["title"][0].ToString();
-                item.url = result["findItemsByKeywordsResponse"][0]["searchResult"][0]["item"][i]["viewItemURL"][0].ToString();
-                item.price = result["findItemsByKeywordsResponse"][0]["searchResult"][0]["item"][i]["sellingStatus"][0]["currentPrice"][0]["__value__"].ToString();
-
-                items.Add(item);
-            }
-
-            ViewBag.ebayResuls = items;
-
-            return View();
         }
     }
 }
---
+++ FinalProject/Models/Buy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinalProject.Models
+{
+    public class Buy
+    {
+        public class EbayItem
+        {
+            public string ProductName { get; set; }
+            public string URL { get; set; }
+            public string Price { get; set; }
+        }
+    }
+}
